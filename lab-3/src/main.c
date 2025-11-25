41e0e55a14d866c1b2fcbaa8a584ddde3e940e9e
Teollan <djteoprofessional@gmail.com>
2025-11-25 02:27:51 +0200

    updated workspace settings

--- lab-3/src/main.c
+++ lab-3/src/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-#include "vector.h"
-#include "matrix.h"
+
 #include "gauss.h"
+#include "matrix.h"
+#include "vector.h"
 
 int main() {
   // double data[] = {
@@ -11,16 +12,12 @@ int main() {
   //    6, 19,  3, 13, 149,
   // };
 
-  double data[] = {
-    2, 1, 1,
-    1, 3, 2,
-    1, 0, 0
-  };
+  double data[] = {2, 1, 1, 1, 3, 2, 1, 0, 0};
 
   Matrix A = createMatrix(3, 3, data);
 
   // Compute inverse
-  Matrix invA = getInverseMatrix(A);
+  // Matrix invA = getInverseMatrix(A);
 
   // Multiply A * invA
   // Matrix product = multiplyMatrices(A, invA);
@@ -29,7 +26,7 @@ int main() {
   printf("Original Matrix A:\n%s\n", matrixToString(A));
 
   // Print inverse
-  printf("Inverse Matrix invA:\n%s\n", matrixToString(invA));
+  // printf("Inverse Matrix invA:\n%s\n", matrixToString(invA));
 
   // Print product
   // printf("Product A * invA:\n%s\n", matrixToString(product));
